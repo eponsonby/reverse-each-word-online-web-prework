57257a9503fda5114de86ec09f336287feb466d8
Erin Ponsonby <erinponsonby@gmail.com>
2019-03-22 17:11:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split
-  sentence_array.each do |element|
-    element.reverse!
-    
+  sentence_array.each do |word|
+    word.reverse!
   end
+  puts sentence_array
 end
